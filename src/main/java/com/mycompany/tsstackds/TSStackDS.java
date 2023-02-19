dab005639e95a444d478cd1dbf1e3343e4756d1f
nka2001 <89935053+nka2001@users.noreply.github.com>
2023-02-19 13:05:16 -0500

    TSStack - Code Template Out

--- src/main/java/com/mycompany/tsstackds/TSStackDS.java
+++ src/main/java/com/mycompany/tsstackds/TSStackDS.java
@@ -4,9 +4,22 @@ package com.mycompany.tsstackds;
  *
  * @author nicka
  */
-public class TSStackDS {
+public class TSStackDS extends Thread {
 
+    public static int amount = 0;
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        TSStackDS thread = new TSStackDS();
+        thread.start();
+        System.out.println(amount);
+        amount++;
+        System.out.println(amount);
+      
+
+    }
+
+    @Override
+    public void run() {
+      amount++;
     }
 }
--- src/main/java/com/mycompany/tsstackds/ThreadSafeStack.java
+++ src/main/java/com/mycompany/tsstackds/ThreadSafeStack.java
@@ -1,9 +1,65 @@
 package com.mycompany.tsstackds;
 
 /**
+ * This class will contain a thread safe stack implementation
  *
  * @author nicka
+ * @param <T>
  */
-public class ThreadSafeStack {
-    
+public class ThreadSafeStack<T extends Comparable<T>> {
+
+    public ThreadSafeStack() {
+
+    }
+
+    /**
+     * Peek operation will look but not remove the top element in the stack
+     * synchronized is used for thread safety
+     *
+     * @return
+     */
+    public synchronized T peek() {
+        return null;
+    }
+
+    /**
+     * pop operation will look at the top element and remove it from the stack
+     * synchronized is used for thread safety
+     *
+     * @return
+     */
+    public synchronized T pop() {
+        return null;
+    }
+
+    /**
+     * push operation, will add addMe to the stack in a LIFO manner, synchronized
+     * is used for thread safety
+     *
+     * @param addMe
+     */
+    public synchronized void push(T addMe) {
+
+    }
+
+    /**
+     * isEmpty will return true if the stack is empty, synchronized is used for
+     * thread safety
+     *
+     * @return
+     */
+    public synchronized boolean isEmpty() {
+        return false;
+    }
+
+    /**
+     * size will return the size of the stack, synchronized is used for thread
+     * safety
+     *
+     * @return
+     */
+    public synchronized int size() {
+        return -1;
+    }
+
 }
